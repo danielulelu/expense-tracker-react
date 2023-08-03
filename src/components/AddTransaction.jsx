9a05cf0b39afc429e18205ee6744d32bc87cc125
Eleven <danielulelu@gmail.com>
2023-08-03 12:32:46 +0300

    Expense tracker using react js

---
+++ src/components/AddTransaction.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
+
+
+export const AddTransaction = () => {
+    const [text, setText] = useState('');
+    const [amount, setAmount] = useState('');
+
+    const { addTransaction } = useContext(GlobalContext);
+
+    const onSubmit  = e => {
+        e.preventDefault();
+
+        const newTransaction  = {
+            id: Math.floor(Math.random() * 10000000),
+            text,
+            amount: +amount
+        }
+
+        addTransaction(newTransaction);
+    }
+
+  return (
+    <div>
+        <h3>AddTransaction</h3>
+        <form onSubmit={onSubmit}>
+            <div className="form-control">
+                <label htmlFor="text">Text</label>
+                <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..." />
+            </div>
+            <div className="form-control">
+                <label htmlFor="amount">
+                    Amount <br /> (negative - expense, positive - income)
+                </label>
+                <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount..." />
+                <button className="btn">Add transaction</button>
+            </div>
+        </form>
+    </div>
+  )
+}
